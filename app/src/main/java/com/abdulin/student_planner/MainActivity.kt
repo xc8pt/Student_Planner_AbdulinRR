7cd9b72ffbc90c9327fb3f9ee34ada48e8f65979
xc8pt <brolike636@gmail.com>
2026-03-16 15:59:52 +0300

    Initial commit

---
+++ app/src/main/java/com/abdulin/student_planner/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.abdulin.student_planner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abdulin.student_planner.ui.theme.Student_PlannerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Student_PlannerTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Student_PlannerTheme {
+        Greeting("Android")
+    }
+}
